5a29aef9e782139e0326df02d6702ae8dd80152e
IsaacLau <1090969255@qq.com>
2021-03-04 14:38:48 +0800

    1、standard code,import resort,delete empty line,add blank

--- app/src/main/java/com/example/androiddevchallenge/ui/ui/PandaList.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/ui/PandaList.kt
@@ -31,14 +31,14 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.androiddevchallenge.ui.data.Panda
 import com.example.androiddevchallenge.ui.MainViewModel
+import com.example.androiddevchallenge.ui.data.Panda
 import com.example.androiddevchallenge.ui.ui.theme.MyTheme
 
 @Composable
