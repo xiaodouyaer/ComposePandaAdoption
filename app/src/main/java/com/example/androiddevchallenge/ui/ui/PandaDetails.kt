85b50f54ef9a8b95e920b2908c8cf4f7790b9531
IsaacLau <1090969255@qq.com>
2021-03-04 12:10:13 +0800

    1、standard code,delete wildcard import

--- app/src/main/java/com/example/androiddevchallenge/ui/ui/PandaDetails.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/ui/PandaDetails.kt
@@ -17,7 +17,12 @@ package com.example.androiddevchallenge.ui.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
